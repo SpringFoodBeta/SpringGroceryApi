a0e8d8e380afddc90f3f50207989e9b72d488638
Tsumbedzo_Nevhutalu <vanessatn96@gmail.com>
2023-06-05 10:46:34 +0200

    creation of the model with its attibutes and relevent dependencies

--- food_service/src/main/java/com/foodapi/food_service/FoodServiceApplication.java
+++ food_service/src/main/java/com/foodapi/food_service/FoodServiceApplication.java
@@ -1,4 +1,13 @@
 package com.foodapi.food_service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FoodServiceApplication {
+
+    // main method responsible for running the whole or the entire project
+    public static void main(String[] args) {
+        SpringApplication.run(FoodServiceApplication.class, args);
+    }
 }
