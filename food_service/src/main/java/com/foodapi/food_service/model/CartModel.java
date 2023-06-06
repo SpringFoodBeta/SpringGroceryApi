b0676d366f10894e5a692028abeb6d07ae99aaec
Tsumbedzo_Nevhutalu <vanessatn96@gmail.com>
2023-06-06 16:31:32 +0200

    added cart model not working

--- food_service/src/main/java/com/foodapi/food_service/model/CartModel.java
+++ food_service/src/main/java/com/foodapi/food_service/model/CartModel.java
@@ -1,4 +1,37 @@
 package com.foodapi.food_service.model;
 
+import jakarta.persistence.*;
+
+
+@Table(name= "Cart")
+
 public class CartModel {
-}
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+
+        @Column(name = "cartId")
+        private Long cartId;
+
+        @Column(name = "cartDetails")
+        private String cartDetails;
+
+        @JoinColumn(name = "productId")
+        private Long productId;
+
+        @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+        @JoinColumn(name = "customerId")
+        private Long customerId;
+
+        public CartModel() {
+
+        }
+
+        public CartModel(Long cartId, String cartDetails, Long productId, Long customerId){
+            this.cartId = cartId;
+            this.cartDetails = cartDetails;
+            this.productId = productId;
+            this.customerId = customerId;
+
+        }
+
+    }
