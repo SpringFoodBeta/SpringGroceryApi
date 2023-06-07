df8d493df0b111bea586dc6522ce7eefb7ed332d
Tsumbedzo_Nevhutalu <vanessatn96@gmail.com>
2023-06-07 09:42:10 +0200

    changed the whole code of the Cart Model due to joining  of relationships and the getters and setters

---
+++ food_service/src/main/java/com/foodapi/food_service/controller/CartController.java
@@ -0,0 +1,4 @@
+package com.foodapi.food_service.controller;
+
+public class CartController {
+}
--- food_service/src/main/java/com/foodapi/food_service/model/CartModel.java
+++ food_service/src/main/java/com/foodapi/food_service/model/CartModel.java
@@ -2,36 +2,68 @@ package com.foodapi.food_service.model;
 
 import jakarta.persistence.*;
 
-
+@Entity
 @Table(name= "Cart")
+class CartModel {
 
-public class CartModel {
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cart_id;
 
-        @Column(name = "cartId")
-        private Long cartId;
+    @Column(name = "cart_details")
+    private String cart_details;
 
-        @Column(name = "cartDetails")
-        private String cartDetails;
+    @JoinColumn(name = "product_id")
+    private ProductModel product;
 
-        @JoinColumn(name = "productId")
-        private Long productId;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private CustomerModel customer;
 
-        @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-        @JoinColumn(name = "customerId")
-        private Long customerId;
-
-        public CartModel() {
-
-        }
-
-        public CartModel(Long cartId, String cartDetails, Long productId, Long customerId){
-            this.cartId = cartId;
-            this.cartDetails = cartDetails;
-            this.productId = productId;
-            this.customerId = customerId;
-
-        }
-
+    public CartModel() {
     }
+
+    public CartModel(Long cart_id, String cart_details, ProductModel product, CustomerModel customer) {
+        this.cart_id = cart_id;
+        this.cart_details = cart_details;
+        this.product = product;
+        this.customer = customer;
+    }
+
+
+    public Long getCart_id() {
+        return cart_id;
+    }
+
+    public void setCart_id(Long cart_id) {
+        this.cart_id = cart_id;
+    }
+
+    public String getCart_details() {
+        return cart_details;
+    }
+
+    public void setCart_details(String cart_details) {
+        this.cart_details = cart_details;
+    }
+
+    public ProductModel getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductModel product) {
+        this.product = product;
+    }
+
+    public CustomerModel getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerModel customer) {
+        this.customer = customer;
+    }
+}
+
+
+
+
